1dc89f60309c6ade56f887481f695c587bf0609d
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-04-10 00:27:32 -0500

    añadiendo detalles fecha y texto

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Client} from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import {Mensaje} from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,8 @@ export class ChatComponent implements OnInit {
 
   private client: Client;
   conectado: boolean = false;
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
   constructor() { }
 
   ngOnInit() {
@@ -22,6 +25,13 @@ export class ChatComponent implements OnInit {
     this.client.onConnect = (frames) => {
       console.log('Conectados: ' + this.client.connected + ' : ' + frames);
       this.conectado = true;
+
+      this.client.subscribe('/chat/mensaje', e => {
+        const mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
     };
     // para conectadornos
     this.client.onDisconnect = (frames) => {
@@ -37,6 +47,11 @@ export class ChatComponent implements OnInit {
 
   desConectar(): void {
     this.client.deactivate();
+  }
+
+  enviarMensaje(): void {
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    this.mensaje.texto = '';
   }
 
 }
